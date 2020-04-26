8d62a22c311fc5b5d663ceb2b45a77509cd18ffb
hbliang <peter695995@gmail.com>
2020-04-26 22:21:28 +0800

    123

--- preprocess.sh
+++ preprocess.sh
@@ -4,9 +4,9 @@
 
 
 echo "make fake wav.scp, utt2spk, spk2utt, text"
-
-wav_data=$1
-test_data_dir=$2
+num_of_jobs=$1
+wav_data=$2
+test_data_dir=$3
 current_path=`pwd`
 if [ -d $test_data_dir ]; then
    echo "The dir '$test_data_dir' exists."
@@ -26,6 +26,6 @@ cp $test_data_dir/utt2spk $test_data_dir/spk2utt
 cp $test_data_dir/utt2spk $test_data_dir/text
 
 echo "make fbank & compute cmvn"
-steps/make_fbank.sh --nj 10 $test_data_dir $test_data_dir/log $test_data_dir/fbank
+steps/make_fbank.sh --nj $num_of_jobs $test_data_dir $test_data_dir/log $test_data_dir/fbank
 steps/compute_cmvn_stats.sh $test_data_dir $test_data_dir/log $test_data_dir/cmvn
 echo "done!"
