3810302a5482fa290bc5b0af6018149664258017
hbliang <peter695995@gmail.com>
2020-04-26 22:15:14 +0800

    1

--- data.sh
+++ data.sh
@@ -2,4 +2,4 @@ gdown --id 1rrT3YJZ1EgXW4Yyll-R-evblpQP-N1lZ
 gdown --id 1OQeR2AqwB72rgeX8_P02Gma2Xwwf5B8Z
 tar zxf HCLG-120k-order2.tar.gz
 tar zxf LM_carpa.tar.gz
-mkdir -p wav data result
+mkdir -p data result
